75391fcf282660e846705a411b7dfac054a76a9b
JoseDanielNovaMuñozCAMPUS <josedanielnova2027@gmail.com>
2023-09-19 12:14:19 -0500

    feat: :construction: login frontend
    realice dos botones que da posibilidad de entrar por discord o como administrador con react

---
+++ src/components/app.jsx
@@ -0,0 +1,11 @@
+import Login from "./app/login"
+
+const App = () =>{
+    return(
+    <div>
+        <Login/>
+    </div>
+    )
+}
+
+export default App
---
+++ src/components/app/login.jsx
@@ -0,0 +1,14 @@
+const Login = () =>{
+    return(
+        <div className='div-centrado'>
+            <button className="boton-grande">
+                <img src="https://i0.wp.com/spaniardperformance.com/wp-content/uploads/2023/04/discord-logo.png?ssl=1" alt="" />
+            </button>
+            <button className="boton-grande">
+                <img src="https://cdn-icons-png.flaticon.com/512/1954/1954541.png" alt="" />
+            </button>
+        </div>
+    )
+}
+
+export default Login
